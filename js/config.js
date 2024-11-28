df29120ce8a1683249ae30f731b441c11a76d98c
AnisLouail19 <162435421+AnisLouail19@users.noreply.github.com>
2024-11-28 10:29:05 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_tVeR2FUfW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4giv1726dij0runblpjulp4reh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
